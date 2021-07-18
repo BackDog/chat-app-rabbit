dec3dceb94ad07c2a073554d146b4410faa5fc43
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2021-07-18 07:29:14 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('A client connected');
+    ws.on('message', function(message) {
+       wss.clients.forEach((client) => {
+           client.send(JSON.stringify({emit:"usersUpdate",data:socketsStatus}));
+       });
+    });
+  
+    ws.on('close', () => function () {
+      console.log('A client disconnected');
+    });
+});
+setInterval(function() { 
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify({}));
+    }); 
+}, 1000);
